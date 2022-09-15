459ac769046a0b27dbcf5ddb37a7a9dc6e7057ad
Andrey Tarasenko <88667005+TarAndr@users.noreply.github.com>
2022-09-15 10:55:18 +0400

    Update and rename Fibo.txt to Fibo.cpp

---
+++ Fibo.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main() {
+    
+    int n, a = 0, b = 1;
+    
+    std::cin >> n;
+    
+    while (n--) {
+        b += a;
+        a = b - a;
+    }
+    
+    std::cout << a;
+}
