1d1446d4e195fe75831f1b000ce87ab11fb766d6
Andrey Tarasenko <88667005+TarAndr@users.noreply.github.com>
2022-09-17 21:16:35 +0400

    Update and rename Palindrom.txt to Palindrom.cpp

--- Palindrom.cpp
+++ Palindrom.cpp
@@ -21,19 +21,24 @@ yes
 
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 int main()
 {
     bool pal = 1;
     string str;
+    
     getline(cin, str);
+    
     for(int i = 0; i < str.size() / 2; i++) {
         if(str[i] != str[str.size() - i - 1]) {
             pal = 0;
             break;
         }
     }
+    
     cout << ((pal) ? "yes" : "no") << endl;
+    
     return 0;
 }
