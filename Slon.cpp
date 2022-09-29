b7ea94805b4ca76bfe89c5a0f619f33d21f137f3
Andrey Tarasenko <88667005+TarAndr@users.noreply.github.com>
2022-09-29 10:51:05 +0400

    Update and rename Slon.txt to Slon.cpp

--- Slon.cpp
+++ Slon.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+
 using namespace std;
+
 int main() {
-  // put your code here
+
     int i_from, j_from, i_to, j_to;
+  
     cin >> i_from >> j_from >> i_to >> j_to;
+  
     if((i_from - i_to) * (i_from - i_to) == (j_from - j_to) * (j_from - j_to)) {
         cout << "YES" << endl;
     } else {
             cout << "NO" << endl;
     }
+  
   return 0;
 }
