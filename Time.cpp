9c7be5856436a95660c64ea3859704ff507b3b78
Andrey Tarasenko <88667005+TarAndr@users.noreply.github.com>
2022-09-29 11:03:32 +0400

    Update and rename Tests-1.txt to Time.cpp

--- Time.cpp
+++ Time.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+
 using namespace std;
+
 int main() {
-    // put your code here
     int h, mm, ss, S;
+    
     cin >> S;
+    
     h = (S / 3600) % 24;
     mm = (S / 60) % 60;
     ss = S % 60;
+    
     cout << h << ":" << mm / 10 << mm % 10 << ":" << ss / 10 << ss % 10;
+    
     return 0;
 }
 
