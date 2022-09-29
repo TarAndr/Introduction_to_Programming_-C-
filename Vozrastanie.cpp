7e2b433711e5fc80687a312a7a784a5858a1c554
Andrey Tarasenko <88667005+TarAndr@users.noreply.github.com>
2022-09-29 13:13:15 +0400

    Update and rename Vozrastanie.txt to Vozrastanie.cpp

---
+++ Vozrastanie.cpp
@@ -0,0 +1,48 @@
+﻿/*
+Дано три числа. Упорядочите их в порядке неубывания.
+
+Формат входных данных
+Вводятся три числа.
+Формат выходных данных
+Выведите ответ на задачу.
+Sample Input:
+
+1
+2
+1
+Sample Output:
+
+1 1 2
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B, C, Temp;
+  
+    cin >> A >> B >> C;
+  
+    if(A > B) {
+        Temp = A;
+        A = B;
+        B = Temp;
+    }
+  
+    if(A > C) {
+        Temp = A;
+        A = C;
+        C = Temp;
+    }
+  
+    if(B > C) {
+        Temp = B;
+        B = C;
+        C = Temp;
+    }
+  
+    cout << A << " " << B << " " << C << endl;
+  
+  return 0;
+}
