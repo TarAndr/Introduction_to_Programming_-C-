ed609335f5626a3b537adc6075ce2ddbc123e422
Andrey Tarasenko <88667005+TarAndr@users.noreply.github.com>
2022-09-29 13:16:20 +0400

    Update and rename longest_word.txt to longest_word.cpp

---
+++ longest_word.cpp
@@ -0,0 +1,53 @@
+﻿/*
+
+Найдите в данной строке самое длинное слово и выведите его.
+
+Входные данные
+
+Вводится одна строка. Слова в ней отделены одним пробелом.
+
+Выходные данные
+
+Выведите самое длинное слово. Если таких слов несколько, то выводить нужно, которое встречается раньше.
+
+Sample Input:
+
+Everyone of us has all we need
+Sample Output:
+
+Everyone
+
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string longest_word() {
+    int index = 0, len = 0, temp = 0;
+    string str;
+    getline(cin, str);
+    str += ' ';
+    for(int i = 0; i < str.size(); i++) {
+        if(str[i] == ' ') {
+            if(i - temp > len) {
+                len = i - temp;
+                index = temp;
+            }
+            temp = i + 1;
+        }
+    }
+    string word;
+    for(int i = index; i < index + len; i++) {
+        word += str[i];
+    }
+    return word;
+}
+
+int main()
+{
+    cout << longest_word();
+    
+    return 0;
+}
